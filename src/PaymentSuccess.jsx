3d80fa4b698862dbe76b1420b2a0451524eb5090
Priyansh shah <89834223+shah-priyansh@users.noreply.github.com>
2024-01-19 17:27:49 +0530

    Code upload
    Code upload

---
+++ src/PaymentSuccess.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PaymentSuccess = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      Payment Successfull
+    </div>
+  );
+};
+
+export default PaymentSuccess;
